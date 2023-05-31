3274a01194dd105815f5aeceb6d7a1ef0cf7ea6f
Altynai Ferrum <pr.galaxy812@gmail.com>
2023-05-31 11:38:22 +0600

    30.05.23 classwork notes app part one add styles,themes

---
+++ app/src/main/java/com/jyldyzferr/notesapp/SimpleNoteDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.jyldyzferr.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SimpleNoteDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple_note_details)
+    }
+}
